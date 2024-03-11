31022019c03089c3012af5cc24c95173ec808e2a
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-03-11 12:04:30 +0530

    this is the initial setup for Ai project

---
+++ NewAI/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.5rem] lg:pt-[5.5rem] overflow-hidden">
+        
+      </div>
+      
+    </>
+  );
+};
+
+export default App;
