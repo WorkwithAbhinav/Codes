4d6b778cedd6689fe56918a2a7a20c7c1491b39d
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-03-12 14:04:30 +0530

    Creating the about page sec

--- NewAI/src/components/About.jsx
+++ NewAI/src/components/About.jsx
@@ -64,7 +64,27 @@ export default function About() {
             </div>
           </div>
         </div>
-        
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
