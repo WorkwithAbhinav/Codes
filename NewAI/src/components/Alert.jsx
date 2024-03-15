845aa060622ca811bfe57e0199f9072c023a2f72
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-03-15 12:04:30 +0530

    Alerts completed

--- NewAI/src/components/Alert.jsx
+++ NewAI/src/components/Alert.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+// Alert.js
+import React from "react";
 
-const Alert = () => {
+export default function Alert(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    props.msgAlert && (
+      <div className={`alert alert-${props.msgAlert.type} alert-dismissible fade show`} role="alert">
+        <strong>{props.msgAlert.type}</strong> {props.msgAlert.message} {/* Added space between type and message */}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    )
+  );
 }
-
-export default Alert
