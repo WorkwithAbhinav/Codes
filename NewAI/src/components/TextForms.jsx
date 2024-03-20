9ca08c8167709e25f7d5ee5da73794cb4b684005
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-03-20 12:04:30 +0530

    designs

--- NewAI/src/components/TextForms.jsx
+++ NewAI/src/components/TextForms.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
 
-const TextForms = () => {
+export default function TextForms(props) {
+
+
   return (
+    <>
     <div>
-      
-    </div>
-  )
-}
+      <div className="mb-4">
+        <h1>{props.heading}</h1>
+        {/* <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/> */}
+      </div>
+      <div className="mb-4">
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="8"
+          value={Text}
+          onClick={hovertext}
+          onChange={handelchange}
+        ></textarea>
+      </div>
 
-export default TextForms
+    </div>
+
+    </>
+  );
+}
