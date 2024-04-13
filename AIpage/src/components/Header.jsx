5e7bd8be0db9d3032975e579665df69f578db495
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-04-13 20:20:30 +0530

    Heading Components

--- AIpage/src/components/Header.jsx
+++ AIpage/src/components/Header.jsx
@@ -1,3 +1,13 @@
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import { useLocation } from "react-router-dom";
+import Buttons from "./Buttons";
+import MenuSvg from '../assets/svg/MenuSvg';
+import { HamburgerMenu } from './design/Header';
+import { useState } from "react";
+import { disablePageScroll } from "scroll-lock";
+import { enablePageScroll } from "scroll-lock";
+
 import React from 'react'
 
 const Header = () => {
