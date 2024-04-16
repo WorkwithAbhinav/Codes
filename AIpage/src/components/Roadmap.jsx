c1e6545fd0fa4fe532f62f64ac124cc76353649a
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-04-17 03:35:01 +0530

    Roadmap compo

--- AIpage/src/components/Roadmap.jsx
+++ AIpage/src/components/Roadmap.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import Section from "./Section";
+import Heading from "./Heading";
+import { roadmap } from "../constants";
+import { check2, grid, loading1 } from "../assets";
+import Tagline from "./Tagline";
+import { Gradient } from "./design/Roadmap";
+import Buttons from "./Buttons";
 
 const Roadmap = () => {
   return (
-    <div>
-      This is roadmap
-    </div>
-  )
-}
+    <Section className="overflow-hidden" id="roadmap">
+    <div className="container md:pb-10">
+      <Heading tag="Ready to get started" title="What we are working on" />
 
-export default Roadmap
+      <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+        {roadmap.map((item) => {
+          const status = item.status === "done" ? "Done" : "In progress";
+
+          return (
+            <div
+              className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                item.colorful ? "bg-conic-gradient" : "bg-n-6"
+              }`}
+              key={item.id}
+            >
+              <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                <div className="absolute top-0 left-0 max-w-full">
+                  <img
+                    className="w-full"
+                    src={grid}
+                    width={550}
+                    height={550}
+                    alt="Grid"
+                  />
+                </div>
+                
+              </div>
+            </div>
+          );
+        })}
+
+        <Gradient />
+      </div>
+
+      
+    </div>
+  </Section>
+
+  );
+};
+
+export default Roadmap;
