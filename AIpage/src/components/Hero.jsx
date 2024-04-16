fbcd25eb5b1779a885070f1c766698c24e003cda
Abhinav <114948002+WorkwithAbhinav@users.noreply.github.com>
2024-04-16 17:39:30 +0530

    Hero Section

--- AIpage/src/components/Hero.jsx
+++ AIpage/src/components/Hero.jsx
@@ -45,7 +45,28 @@ const Hero = () => {
           <div className="relative z-1 rounded-2xl bg-conic-gradient p-0.5">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
-             
+              <div className="rounded-b-[0.9rem] overflow-hidden aspect-[33/40] md:aspect-[688/490]  lg:aspect-[1024/490] ">
+                <img
+                  src={robot}
+                  className="w-full md:scale-[1] scale-[1.7] translate-y-[8%]
+                    md:translate-y-[10%] lg:-translate-y-[23%]
+                  "
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"/>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+              </div>
             </div>
             <Gradient />
           </div>
@@ -54,7 +75,6 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
-        <CompanyLogos className="hidden lg:block z-10 mt-20 relative"/>
       </div>
       <BottomLine/>
     </Section>
